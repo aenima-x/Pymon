bf6ab543defc58233d6cc8f0c29ebff453b9391e
Nicolas Rebagliati <nicolas.rebagliati@aenima-x.com.ar>
2013-10-22 10:46:14 -0300

    Fix python 2.4 compatibility issue

--- pymon/__init__.py
+++ pymon/__init__.py
@@ -9,7 +9,10 @@ def get_version():
     """Returns a PEP 386-compliant version number from VERSION."""
     assert VERSION[3] in ('alpha', 'beta', 'rc', 'final')
     sub = ''
-    parts = 2 if VERSION[2] == 0 else 3
+    if VERSION[2]:
+        parts = 2
+    else:
+        parts = 3
     main = '.'.join(str(x) for x in VERSION[:parts])
     if VERSION[3] != 'final':
         mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
