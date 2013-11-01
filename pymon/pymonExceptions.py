eccca3058a3e8656fa4cc845c6bd4e9128d0166f
Nicolas Rebagliati <nicolas.rebagliati@aenima-x.com.ar>
2013-11-01 11:42:54 -0300

    Inherits form Exception instead for BaseExeption

--- pymon/pymonExceptions.py
+++ pymon/pymonExceptions.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 
 
-class InvalidAddressError(BaseException):
+class InvalidAddressError(Exception):
     def __init__(self, address):
         super(InvalidAddressError, self).__init__()
         self.message = "The address %s is invalid!!" % address
 
 
-class InvalidPortError(BaseException):
+class InvalidPortError(Exception):
     def __init__(self, port):
         super(InvalidPortError, self).__init__()
         self.message = "The port %s is invalid!!" % port
 
 
-class InvalidPath(BaseException):
+class InvalidPath(Exception):
     def __init__(self, path):
         super(InvalidPath, self).__init__()
         self.message = "The path %s is invalid!!" % path
 
 
-class ClientMissingInfoError(BaseException):
+class ClientMissingInfoError(Exception):
     def __init__(self, missingInfo):
         super(ClientMissingInfoError, self).__init__()
         self.message = u"Missing %s information" % missingInfo
